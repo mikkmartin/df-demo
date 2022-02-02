45ecd0d72815c39df79a534e081600001f893a7a
Mikk Martin <mikk.martin@gmail.com>
2022-02-02 06:04:28 +0200

    Update rootUrl.ts

--- utils/rootUrl.ts
+++ utils/rootUrl.ts
@@ -1,4 +1,5 @@
 export const getRootUrl = (req): string => {
   const protocol = req.headers['x-forwarded-proto'] || req.connection.encrypted ? 'https' : 'http'
-  return `${protocol}://${req.headers.host}`
+  const host = req.headers['x-forwarded-host'] || req.headers.host
+  return `${protocol}://${host}`
 }
